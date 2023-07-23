300309db68c0b79f94ef77ff6da5e3297a25d92f
jamshed391 <123306549+jamshed391@users.noreply.github.com>
2023-07-22 22:50:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const downloadButton = document.getElementById('downloadButton');
-const apkFileUrl = 'https://pacific.us.hostns.io:2083/cpsess3933711991/frontend/jupiter/filemanager/index.html'; // Replace with the actual URL of your APK file.
+const apkFileUrl = 'https://ydray.com/get/t/16900591016571fuys55930ed3679ccb'; // Replace with the actual URL of your APK file.
 
 downloadButton.addEventListener('click', () => {
   // Create a temporary anchor element to initiate the download
   const downloadLink = document.createElement('a');
   downloadLink.href = apkFileUrl;
-  downloadLink.download = 'https://pacific.us.hostns.io:2083/cpsess3933711991/frontend/jupiter/filemanager/index.html'; // Set the desired filename for the downloaded APK file
+  downloadLink.download = 'https://ydray.com/get/t/16900591016571fuys55930ed3679ccb'; // Set the desired filename for the downloaded APK file
 
   // Append the anchor element to the body and trigger the click event
   document.body.appendChild(downloadLink);
